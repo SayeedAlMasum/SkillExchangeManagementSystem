17def042934571eb8ba6fa69bf395071c73863e7
SayeedAlMasum <githubms789@gmail.com>
2025-04-02 18:24:35 +0600

    Committed

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -16,7 +16,7 @@ namespace Web.Pages
 
         public void OnGet()
         {
-            Roles = new List<string> { "Beginner", "Intermediate", "Expert" };
+            Roles = new List<string> { "Student", "Teacher" };
         }
     }
 }
